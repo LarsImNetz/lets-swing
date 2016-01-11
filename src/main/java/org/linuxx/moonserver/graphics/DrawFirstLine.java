f071b04dd2d2712d59b324c6a7f6099f58f7744a
Lars.im.Netz <java4fun@kaffeeschluerfer.com>
2016-01-11 19:11:22 +0100

    Zeichne ein paar mehr Linien

--- src/main/java/org/linuxx/moonserver/graphics/DrawFirstLine.java
+++ src/main/java/org/linuxx/moonserver/graphics/DrawFirstLine.java
@@ -5,12 +5,56 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+class XY {
+	int x;
+	int y;
+
+	int xStep;
+	int yStep;
+
+	static int XMAX = 200;
+	static int YMAX = 200;
+
+	public XY(int x, int y, int xStep, int yStep) {
+		this.x = x;
+		this.y = y;
+		this.xStep = xStep;
+		this.yStep = yStep;
+	}
+
+	public void incXY() {
+		x += xStep;
+		if (x >= XMAX || x <= 0) {
+			xStep = -xStep;
+			x += xStep;
+		}
+
+		y += yStep;
+		if (y >= YMAX || y <= 0) {
+			yStep = -yStep;
+			y += yStep;
+		}
+	}
+}
+
+@SuppressWarnings("serial")
 class DrawPanel extends JPanel {
+
+	XY p1 = new XY(0, 0, 5, 6);
+	XY p2 = new XY(50, 20, 4, 3);
 
 	@Override
 	protected void paintComponent(final Graphics g) {
 		super.paintComponent(g);
-		g.drawLine(10, 10, 100, 50);
+		for (int i = 0; i < 500; i++) {
+			g.drawLine(p1.x, p1.y, p2.x, p2.y);
+			p1.incXY();
+			p2.incXY();
+//			try {
+//				Thread.sleep(1);
+//			} catch (InterruptedException ex) {
+//			}
+		}
 	}
 }
 
@@ -19,7 +63,7 @@ public class DrawFirstLine {
 	public static void main(final String[] args) {
 		final JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(100, 100);
+		f.setSize(200, 200);
 		f.add(new DrawPanel());
 		f.setVisible(true);
 	}
